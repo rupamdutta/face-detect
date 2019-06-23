7cd59f43a6c81c709825619944ae502d4eec20e7
rupamdutta <dutta.rupam1987@gmail.com>
2019-06-24 00:38:50 +0530

    1212

--- src/app/+face-similarity/face-similarity.component.ts
+++ src/app/+face-similarity/face-similarity.component.ts
@@ -11,8 +11,7 @@ export class FaceSimilarityComponent implements OnInit {
 
   threshold = 0.6;
   descriptors = {
-    url1: '', desc1: null,
-    url2: '', desc2: null,
+    url1: '', desc1: null, url2: '', desc2: null
   };
   progress = false;
   facesMatched = null;
@@ -25,6 +24,10 @@ export class FaceSimilarityComponent implements OnInit {
     this.progress = true;
     await faceapi.nets.faceRecognitionNet.loadFromUri(
       environment.baseHref + 'assets/models/face_recognition');
+    await faceapi.nets.ssdMobilenetv1.loadFromUri(
+      environment.baseHref + 'assets/models/ssd_mobilenetv1');
+    await faceapi.nets.faceLandmark68Net.loadFromUri(
+      environment.baseHref + 'assets/models/face_landmark_68');
     this.progress = false;
   }
 
@@ -32,40 +35,40 @@ export class FaceSimilarityComponent implements OnInit {
     filePicker.click();
   }
 
-  async onImageSelect(e, id, filePicker) {
+  async onImageSelect(e, id, filePicker, img) {
     this.progress = true;
     const files = e.target.files;
     if (FileReader && !!files && files.length) {
       const fileReader = new FileReader();
       fileReader.onload = () => {
-        this.setImageDescriptor(fileReader.result, id);
+        this.descriptors['url' + id] = fileReader.result;
+        img.src = fileReader.result;
+        this.setImageDescriptor(id, img);
         filePicker.value = '';
       };
       fileReader.readAsDataURL(files[0]);
     }
   }
 
-  async setImageDescriptor(result, id) {
-    this.descriptors['url' + id] = result;
+  async setImageDescriptor(id, img) {
     this.descriptors['desc' + id] =
-      await faceapi.computeFaceDescriptor(await faceapi.fetchImage(result));
+      await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
     this.progress = false;
   }
 
   async evaluateFaces() {
     this.progress = true;
     const
-      euclideanDistance = faceapi.euclideanDistance(this.descriptors.desc1, this.descriptors.desc2),
+      euclideanDistance =
+        faceapi.euclideanDistance(this.descriptors.desc1.descriptor, this.descriptors.desc2.descriptor),
       distance = faceapi.round(euclideanDistance);
-    alert('Evaluted: ' + distance);
     this.facesMatched = distance <= this.threshold;
     this.progress = false;
   }
 
   reset() {
     this.descriptors = {
-      url1: '', desc1: null,
-      url2: '', desc2: null,
+      url1: '', desc1: null, url2: '', desc2: null
     };
     this.facesMatched = null;
   }
